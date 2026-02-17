67777c372ae3e9f4a15995cc8aeeb7e1dbe92677
Kartikeya <kartikeyayash4@gmail.com>
2026-02-17 16:48:42 +0530

    first commit

---
+++ VisionIdBackend/src/main/java/com/example/VisionIdBackend/VisionIdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.VisionIdBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionIdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionIdBackendApplication.class, args);
+	}
+
+}
